f13c0ec7c37b6a881b8151d35ee65bc56c731485
Rumazor <th3rum2084@gmail.com>
2022-04-21 18:49:17 -0500

    Progreso en el HTML

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,12 @@
 import { getElement } from '../utils.js';
 
+const cartOverlay = getElement('.cart-overlay');
+const cartToggleBtn = getElement('.toggle-cart');
+const cartCloseBtn = getElement('.cart-close');
+
+cartToggleBtn.addEventListener('click',()=> cartOverlay.classList.add('show'));
+cartCloseBtn.addEventListener('click',()=> cartOverlay.classList.remove('show'));
+
+
+
 export const openCart = () => {};
