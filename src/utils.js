ca580ac6d2a8f6b27ee248822bcbf3c825c793c0
Rumazor <th3rum2084@gmail.com>
2022-04-21 04:25:20 -0500

    Comfy Store

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,7 @@
-//   ATTENTION!!!!!!!!!!!
-//   I SWITCHED TO PERMANENT DOMAIN
-//   DATA IS THE SAME JUST A DIFFERENT URL,
-//   DOES NOT AFFECT PROJECT FUNCTIONALITY
 
 const allProductsUrl = 'https://course-api.com/javascript-store-products'
-// temporary single product
-// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+
+
 const singleProductUrl =
   'https://course-api.com/javascript-store-single-product'
 
